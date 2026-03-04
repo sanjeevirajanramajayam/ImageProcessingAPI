0e687bdf3d1cf21f57836e196d126ccf4ca67b18
Sanjeevi Rajan <>
2026-03-04 21:53:54 +0530

    Added frontend.

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="h-screen">
+      <div className="flex justify-center items-center h-full">
+        <div className="border p-5 w-1/4">
+          <div className="text-lg my-1 text-center ">Login</div>
+          <p>Email</p>
+          <input
+            type="email"
+            className="border border-black h-10 w-full p-2 my-2"
+          />
+          <p>Password</p>
+          <input className="border border-black h-10 w-full p-2 my-2" />
+          <button className="bg-black text-white p-2 w-full my-4">Login</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Register() {
+  return (
+    <div className="h-screen">
+      <div className="flex justify-center items-center h-full">
+        <div className="border p-5 w-1/4">
+          <div className="text-2xl my-1 text-center">
+            <b>Register User</b>
+          </div>
+          <p>Email</p>
+          <input
+            type="email"
+            className="border border-black h-10 w-full p-2 my-2"
+          />
+          <p>Password</p>
+          <input className="border border-black h-10 w-full p-2 my-2" />
+          <p>Confirm Password</p>
+          <input className="border border-black h-10 w-full p-2 my-2" />
+          <button className="bg-black text-white p-2 w-full my-4">
+            Register
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
